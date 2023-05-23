656e8473c689c4a9251246e045260ecfdca5022a
danilopiccinni <danilopiccinni@gmail.com>
2023-05-23 15:06:26 +0200

    feat: destroy type

--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -10,8 +10,13 @@
                     <div class="card-body">
                       <h5 class="card-title">{{$type->name}}</h5>
                       <p class="card-text">{{$type->description}}</p>
-                      <a href="{{ route('admin.types.edit', $type) }}" class="card-link">Modifica</a>
-                      <a href="#" class="card-link">Eliminia</a>
+                      <a href="{{ route('admin.types.edit', $type) }}" class="card-link btn btn-primary">Modifica</a>
+                      <form action="{{route('admin.types.destroy', $type)}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger" type="submit">ELIMINA</button>
+
+                      </form>
                     </div>
                   </div>
 
