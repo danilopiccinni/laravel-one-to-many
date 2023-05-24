cf32bfd9efbaeace896d615abfe1ecbe38fc3c10
danilopiccinni <danilopiccinni@gmail.com>
2023-05-24 10:22:30 +0200

    fix: dropForeign

--- database/migrations/2023_05_23_112400_add_type_id_to_projects_table.php
+++ database/migrations/2023_05_23_112400_add_type_id_to_projects_table.php
@@ -28,7 +28,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->dropForeign('projects_types_id_foreign');
+            $table->dropForeign('projects_type_id_foreign');
 
             $table->dropColumn('type_id');
         });
